1c0c0686b1e7a0399988dfd220cbfba708549197
Dimas Satrio <dimassrio@gmail.com>
2020-03-18 21:43:01 +0700

    fixing import

--- src/Transformer/RamenTransformerGenerator.js
+++ src/Transformer/RamenTransformerGenerator.js
@@ -4,6 +4,8 @@ const TransformerAbstract = use('Bumblebee/Transformer');
 const fold_1 = require("@adonisjs/fold");
 const lodash_1 = require("lodash");
 const lodash_inflection_1 = require("lodash-inflection");
+const PlainSerializer = require('adonis-bumblebee/src/Bumblebee/Serializers/PlainSerializer');
+const DataSerializer = require('adonis-bumblebee/src/Bumblebee/Serializers/DataSerializer');
 // import {capitalize} from '../Utilities'
 const isJSON = (str) => {
     return !lodash_1.isError(lodash_1.attempt(JSON.parse, str));
@@ -49,7 +51,8 @@ exports.RamenTransformerGenerator = (m, n = []) => {
                     }
                 }
                 const n = (item.getRelated(element) || {}).$relations ? Object.keys(item.getRelated(element).$relations) : [];
-                return this[type](item.getRelated(element), fold_1.ioc.use(`App/Models/${lodash_1.capitalize(lodash_inflection_1.singularize(item[element]().$relation.foreignTable))}`).transformer
+                const serializer = type === 'item' ? PlainSerializer : DataSerializer;
+                return this.setSerializer(serializer)[type](item.getRelated(element), fold_1.ioc.use(`App/Models/${lodash_1.capitalize(lodash_inflection_1.singularize(item[element]().$relation.foreignTable))}`).transformer
                     ? fold_1.ioc.use(`App/Models/${lodash_1.capitalize(lodash_inflection_1.singularize(item[element]().$relation.foreignTable))}`).transformer
                     : exports.RamenTransformerGenerator(fold_1.ioc.use(`App/Models/${lodash_1.capitalize(lodash_inflection_1.singularize(item[element]().$relation.foreignTable))}`), n));
             };

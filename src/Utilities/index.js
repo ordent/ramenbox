9ea3a91a9b2d0d827735027ee6cdd76c479b343b
ddg00 <arya.ddg00@gmail.com>
2020-07-27 12:53:18 +0700

    add missing capitalFirst and pretify file

--- src/Utilities/index.js
+++ src/Utilities/index.js
@@ -1,15 +1,21 @@
 "use strict";
+
+const _ = require("lodash");
+
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.requestBody = (request) => {
-    return Object.assign({}, request.all(), request.files());
+  return Object.assign({}, request.all(), request.files());
 };
 exports.capitalize = (s) => {
-    return s.charAt(0).toUpperCase() + s.slice(1);
+  return s.charAt(0).toUpperCase() + s.slice(1);
+};
+exports.capitalFirst = (s) => {
+  return _.upperFirst(s);
 };
 exports.requestProperties = (request, properties) => {
-    const result = {};
-    properties.forEach((element) => {
-        result[element] = request[element];
-    });
-    return result;
+  const result = {};
+  properties.forEach((element) => {
+    result[element] = request[element];
+  });
+  return result;
 };

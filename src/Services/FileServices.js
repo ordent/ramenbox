35e8feb129790ca69f46a358df8a7a250d92ee42
ddg00 <arya.ddg00@gmail.com>
2020-08-12 14:40:14 +0700

    drive > floppy

--- src/Services/FileServices.js
+++ src/Services/FileServices.js
@@ -37,7 +37,7 @@ const SobaServices_1 = require("./SobaServices");
 const UndefinedException_1 = require("../Exception/UndefinedException");
 const shortid_1 = require("shortid");
 const mime_types_1 = require("mime-types");
-const Drive = use("Drive");
+const Drive = use("Floppy");
 const fs = use("fs");
 const base64Mime = require("base64mime");
 class FileServices extends SobaServices_1.SobaServices {
